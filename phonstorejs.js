10fec2994167a9b5545138731e298f8efbf9f280
locpham92 <phamquyloc92@gmail.com>
2024-05-29 21:27:39 +0700

    loc

--- phonstorejs.js
+++ phonstorejs.js
@@ -1,93 +1,45 @@
-
-// function showHome() {
-//     axios.get("http://localhost:8080orders").then ((response) => {
-//         let list = response.data;
-//         let html = `<table>
-//         <tr>
-//             <td></td>
-//             <td></td>
-//             <td></td>
-//             <td></td>
-//             <td></td>
-//             <td></td>
-//         </tr>`
-//         for (let i = 0; i < list.length; i++) {
-//             html += `
-//             `
-//         }
-//     })
-// }
-// function showFormAdd() {
-//     axios.get('http://localhost:8080/brands').then((response) => {
-//         let brand = response.data;
-//         let html = `<select id="brand">`;
-//         for (let i = 0; i < brand.length; i++) {
-//             html += `<option value="${list[i].id}">${list[i].name}</option>`;
-//         }
-//         html += `</select>`;
-//         document.getElementById("root").innerHTML = `
-//         <input
-//         ${html}
-//         <button onclick="add()">Enter</button>
-//         `
-//     })
-//
-// }
-// function add() {
-//     let brand = document.getElementById("name").value;
-//
-//     let newOrder = {
-//         "name":name,
-//         "brand" :
-//             {
-//                 id: brandId
-//             }
-//     }
-//     axios.post("http://localhost:8080/orders").then((response) => {
-//     showHome();
-//     })
-// }
-// function saleReport() {
-//     axios.get("http://localhost:8080/sales").then((response) => {
-//
-//     })
-// }
 addOrder();
 function addOrder() {
     axios.get("http://localhost:8080/products").then((response) => {
         let list = response.data;
-        var searchInput = document.querySelector('.search-area .search-bar input')
+        let searchInput = document.querySelector('.search-area .search-bar input');
         searchInput.addEventListener('input', function(e){
             let txtSearch = e.target.value.trim().toLowerCase();
-            var products = document.querySelector('.search-result')
+            let products = document.querySelector('.search-result')
             products.innerHTML=``;
             for (let i = 0; i < list.length; i++) {
-                var newProduct = document.createElement('div');
+                let newProduct = document.createElement('div');
                 newProduct.classList.add('suggest-search');
-                if (list[i].name.toLowerCase().includes(txtSearch) && txtSearch !== null) {
+                if (list[i].name.toLowerCase().includes(txtSearch) && (txtSearch !== '')) {
                     newProduct.innerHTML=`<img src="${list[i].image}">
-                                          <div class="info">
+                                          <div class="info" onclick="addToCart(${list[i].id})">
                                             <div>${list[i].name}</div>
                                             <div>Giá: ${list[i].price}</div>
                                             <div>Tồn kho: ${list[i].quantity}</div>
                                           </div>` ;
+                    products.appendChild(newProduct);
                 }
                 else {
                     newProduct.classList.add('hide');
                 }
-                products.appendChild(newProduct);
             }
-            // list.forEach(item => {
-            //     if(item.name.toLowerCase().includes(txtSearch)) {
-            //         document.getElementById("cart").innerHTML = item.name;
-            //     }
-            //     else {
-            //         document.getElementById("cart").innerHTML = "Không tìm thấy";
-            //     }
-            // })
         })
     })
 }
-function cart() {
-    console.log("1");
+let html = '';
+let count = 0;
+function addToCart(id) {
+    axios.get(`http://localhost:8080/products/${id}`).then ((response) => {
+        let product = response.data;
+        count++;
+        html +=`<div class="infor">
+<i class="fa-solid fa-trash"></i>
+                   <span>${product.name}</span>
+                   <input type="text" value="1">
+                   <span>${product.price}</span>   
+ </div>` ;
+
+    })
+    document.getElementById('cart').innerHTML = html;
 }
+function saleReport() {}
